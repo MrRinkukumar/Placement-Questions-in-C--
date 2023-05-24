2ca8a64ad7725d68acea71c9a2f7048e63b0e5ef
MrRinkuKumar <you@example.com>
2023-05-24 17:32:19 +0530

    modified

--- Questions/11_armstronNumber.cpp
+++ Questions/11_armstronNumber.cpp
@@ -4,12 +4,13 @@
 #include<cmath>
 using namespace std;
 int main(){
-    int n=152;
+    int n=153;
     int d=n;
     int sum=0;
     while(n>0){
         int last = n%10;
-        sum+=(last*last*last);
+        // sum+=(last*last*last);
+        sum+=pow(last,3);
         n=n/10;
     }
     if(sum==d){
