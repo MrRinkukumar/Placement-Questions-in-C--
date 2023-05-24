d8ea0a29c017067859856ec7573ac9dd5754ff60
MrRinkuKumar <you@example.com>
2023-05-24 17:21:43 +0530

    check the number is armstrong or not

---
+++ Questions/11_armstronNumber.cpp
@@ -0,0 +1,22 @@
+// write  a programme to check the number is armstrong or not
+// armstrongNUmber:- the armstrong number is a special number which a special property the sum of  every digit cube is equal to actual number
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n=152;
+    int d=n;
+    int sum=0;
+    while(n>0){
+        int last = n%10;
+        sum+=(last*last*last);
+        n=n/10;
+    }
+    if(sum==d){
+        cout<<"the number is armstrong";
+    }
+    else{
+        cout<<"the number is not armstrong";
+    }
+    return 0;
+}
